90001162f354e0f23f56619ee91cae89cbb20873
tahmdev <qwertiimusic@gmail.com>
2022-04-20 22:22:36 +0200

    Progress on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,27 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = ({setShow}) => {
-  let ref = useRef(null)
+  let [newPage, setNewPage] = useState()
 
+  //set initial sidebar content
+  useEffect(() => {
+    setNewPage(
+        <>
+          <div>
+            Mein Markt
+          </div>
+          <ul className="Alle-Kategorien" role="menu">
+            <li role="none"> <h4> Alle Kategorien </h4></li>
+            <CategoryList setNewPage={setNewPage} data={dummyData} />
+          </ul>
+        </>
+    )
+  }, [])
+
+  //Hide sidebar on click outside
+  let ref = useRef(null)
   useEffect(() => {
     document.addEventListener("mousedown", handleClick)
     return () => document.removeEventListener("mousedown", handleClick)
@@ -17,15 +36,191 @@ const Sidebar = ({setShow}) => {
       setShow(false)
     }, 300);
   }
+
   return(
     <div id="background" className="background">
       <div id="sidebar-wrapper" className="sidebar-wrapper" ref={ref}>
-        
-        <nav aria-label="Alle Kategorien">
-
-        </nav>    
+        <div className="sidebar-top">
+          <img src="./logo.png" className='logo'/>
+          <button className="close-sidebar-btn" aria-label="Alle Kategorien schließen" onClick={handleHide}>
+            <FontAwesomeIcon icon={faXmark}/>
+          </button>
+      </div>
+        {newPage}      
       </div>
     </div>
   )
 }
 export default Sidebar
+
+
+const CategoryList = ({data, setNewPage, setPrevPage, prevPage, newPage}) => {
+
+  const getParent = (data, find) => {
+    let parent = ""
+    let result = ""
+    const search = (data, find) => {
+      if (Array.isArray(data)){
+        data.map(item => {
+          if (item.name === find) result = parent
+          else search(item, find)
+        })
+      }
+      else if (typeof data === "object") {
+        if(data.subs){
+          parent = data
+          search(data.subs, find)
+        } 
+      }
+    }
+    search(data, find)
+    return result
+  }
+  const nextPage = (data) => {
+    let prevPage = getParent(dummyData, data.name)
+    console.log(data)
+    if(!Array.isArray(data)){
+      let page =  <>
+                    <div>
+                      <button onClick={() => nextPage(prevPage ? prevPage : dummyData)}> {prevPage ? `Zurück zu ${prevPage.name}` : "Zurück zur Übersicht" } </button>
+                    </div>
+                    <ul className="Alle-Kategorien" role="menu" key={data.name} >
+                      <CategoryList setNewPage={setNewPage} data={data.subs} />
+                    </ul>
+                  </>
+      setNewPage(page)
+    }
+    else{
+      let page =  <>
+                    <div>
+                      Mein Markt
+                    </div>
+                    <ul className="Alle-Kategorien" role="menu">
+                      <li role="none"> <h4> Alle Kategorien </h4></li>
+                      <CategoryList setNewPage={setNewPage} data={dummyData} />
+                    </ul>
+                  </>
+    setNewPage(page)
+    }
+  }
+  
+  if (data){
+    return(
+      <>
+        {
+          data.map(item => {
+          if (!item.subs){
+            return <li key={item.name} > {item.name} without children </li>
+          }
+          return(
+            <li>
+              <button onClick={e => nextPage(item, e)} key={item.name} >
+                <span> {item.name} </span>
+                <FontAwesomeIcon icon={faAngleRight}/>
+              </button>
+            </li>
+            
+          )
+        })}
+      </>
+    )
+  }
+}
+
+const dummyData = [
+  {
+    name: "1",
+    subs: [
+      {
+        name:"2"
+      },
+      {
+        name:"2w",
+        subs: [
+          {
+            name: "3"
+          },
+          {
+            name: "3a"
+          },
+          {
+            name: "3w",
+            subs: [
+              {
+                name: "4w",
+                subs: [
+                  {
+                    name: "5"
+                  }
+                ]
+              },
+              {
+                name: "4"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  // {
+  //   name: "Angebote & Aktionen",
+  //   subs: [
+  //     {
+  //       name: "Zu Angebote & Aktionen"
+  //     },
+  //     {
+  //       name: "Unser Service",
+  //       subs: [
+  //         {
+  //           name: "Zu Unser Service"
+  //         },
+  //         {
+  //           name: "Miet mich!"
+  //         },
+  //         {
+  //           name: "Fotoservice"
+  //         },
+  //         {
+  //           name: "Aktuelle Gewinnspiele"
+  //         },
+  //         {
+  //           name: "0% Finanzierung"
+  //         },
+  //       ]
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: "Computer & Büro",
+  //   subs: [
+  //     {
+  //       name: "Zu Computer & Büro"
+  //     },
+  //     {
+  //       name: "Notebooks & Zubehör",
+  //       subs: [
+  //         {
+  //           name: "Zu Notebooks & Zubehör"
+  //         },
+  //         {
+  //           name: "Notebooks"
+  //         },
+  //         {
+  //           name: "2in1 Convertibles"
+  //         },
+  //         {
+  //           name: "Ultrabooks"
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       name: "Notebook Kaufberatung"
+  //     },
+  //     {
+  //       name: "Mac Konfigurator"
+  //     },
+  //   ]
+  // }
+  
+]
