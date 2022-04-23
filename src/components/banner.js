80dd2b68e49a896e47f2a7e03c60bde7548d1b75
tahmdev <qwertiimusic@gmail.com>
2022-04-23 15:55:20 +0200

    Added banner component

---
+++ src/components/banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+
+  return (
+    <a href="http://localhost:3000/" className="banner">
+      <div>
+        <h2>Jetzt täglich neu: Unser Deal des Tages</h2>
+        <button tabIndex={-1}> Zum Angebot </button>
+      </div>
+    </a>
+  )
+}
+export default Banner
